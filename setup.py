8038b93856e004c39e8711344804c5b3c933890c
Tekicho <44400110+Tekicho@users.noreply.github.com>
2025-03-19 15:52:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,17 @@
-import glob
-import os
 import os.path as osp
 
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-this_dir = osp.dirname(osp.abspath(__file__))+"/poinntnet2_ops_lib"
-_ext_src_root = osp.join("pointnet_ops_lib/pointnet2_ops", "_ext-src")
-_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-    osp.join(_ext_src_root, "src", "*.cu")
-)
-_ext_headers = glob.glob(osp.join(_ext_src_root, "include", "*"))
+requirements = ["hydra-core==0.11.3", "pytorch-lightning==0.7.1"]
+installable_packages= find_packages()
+print(f"Packages to be installed: {installable_packages}")
 
-requirements = ["torch>=1.4"]
+exec(open(osp.join("pointnet2", "_version.py")).read())
 
-exec(open(osp.join("pointnet2_ops", "_version.py")).read())
-
-#os.environ["TORCH_CUDA_ARCH_LIST"] = "3.7+PTX;5.0;6.0;6.1;6.2;7.0;7.5"
-os.environ["TORCH_CUDA_ARCH_LIST"] = "5.0;6.0;6.1;6.2;7.0;7.5;8.0;8.6" #;8.7;8.9;9.0"
 setup(
-    name="pointnet2_ops",
+    name="pointnet2",
     version=__version__,
     author="Erik Wijmans",
     packages=find_packages(),
     install_requires=requirements,
-    ext_modules=[
-        CUDAExtension(
-            name="pointnet2_ops._ext",
-            sources=_ext_sources,
-            extra_compile_args={
-                "cxx": ["-O3"],
-                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
-            },
-            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
-    include_package_data=True,
 )
