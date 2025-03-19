2ffa7e603e170d973b2e04c6f3aeed146145f24e
Tekicho <44400110+Tekicho@users.noreply.github.com>
2025-03-19 15:13:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(f"Packages to be installed: {installable_packages}")
 exec(open(osp.join("pointnet2", "_version.py")).read())
 
 setup(
-    name="pointnet2_ops_lib",
+    name="pointnet2",
     version=__version__,
     author="Erik Wijmans",
     packages=find_packages(),
