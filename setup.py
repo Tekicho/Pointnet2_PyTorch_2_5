49b23e55794660d7f8a942117fbf397c266ddb73
Tekicho <44400110+Tekicho@users.noreply.github.com>
2025-03-19 15:08:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ print(f"Packages to be installed: {installable_packages}")
 exec(open(osp.join("pointnet2", "_version.py")).read())
 
 setup(
-    #name="pointnet2",
-    name="Pointnet2_PyTorch_2_5",
+    name="pointnet2_ops_lib",
     version=__version__,
     author="Erik Wijmans",
     packages=find_packages(),
-    install_requires=[] #requirements,
+    install_requires=requirements,
 )
