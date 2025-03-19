2ac3d39547b0c68275b7878e7e932a0c11f1aee2
Tekicho <44400110+Tekicho@users.noreply.github.com>
2025-03-19 15:53:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import os.path as osp
 from setuptools import find_packages, setup
 
 requirements = ["hydra-core==0.11.3", "pytorch-lightning==0.7.1"]
-installable_packages= find_packages()
-print(f"Packages to be installed: {installable_packages}")
-
 exec(open(osp.join("pointnet2", "_version.py")).read())
 
 setup(
